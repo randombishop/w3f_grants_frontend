20aaf90c05d1c5e5040f4fc912055b8a04c66d37
randombishop <nabil.abdellaoui@expr-research.com>
2023-01-23 12:59:16 -0500

    Grants list

--- app/src/components/grants/Grants.js
+++ app/src/components/grants/Grants.js
@@ -9,14 +9,40 @@ import GrantsList from './GrantsList' ;
 
 export default class Grants extends React.Component {
 
-  render() {
+  constructor(props) {
+    super(props) ;
+    this.state = {
+        grantsAll: null,
+        grantsFiltered: null
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchData() ;
+  }
+
+  fetchData = () => {
+    const self = this ;
+    const url = 'http://localhost:8888/grants';
+    fetch(url)
+    .then(res => res.json())
+    .then(self.updateData)
+    .catch(err => {alert(err)});
+  }
+
+  updateData = (data) => {
+    const sorted = data.sort((a, b) => b.status.acceptDate-a.status.acceptDate);
+    this.setState({grantsAll:sorted, grantsFiltered:sorted}) ;
+  }
+
+  render = () => {
       return (
         <Grid container spacing={2}>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <GrantsFilters />
             </Grid>
-            <Grid item xs={8}>
-              <GrantsList />
+            <Grid item xs={9}>
+              <GrantsList grants={this.state.grantsFiltered} />
             </Grid>
         </Grid>
       );
--- app/src/components/grants/GrantsList.js
+++ app/src/components/grants/GrantsList.js
@@ -1,15 +1,73 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-
+import TableContainer from '@mui/material/TableContainer';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 export default class GrantsList extends React.Component {
+
+
+
+  renderLoading = () => {
+    return <Typography component="div" variant="h5">Loading...</Typography>
+  }
+
+  renderReady = () => {
+    return (
+        <React.Fragment>
+            <Typography component="div" variant="h5">Grants: {this.props.grants.length}</Typography>
+            <br/>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="center"><b>Team / Project</b></TableCell>
+                        <TableCell style={{minWidth:'110px'}} align="center"><b>Started On</b></TableCell>
+                        <TableCell align="center"><b>Level</b></TableCell>
+                        <TableCell align="center"><b>Cost</b></TableCell>
+                        <TableCell align="center"><b>Currency</b></TableCell>
+                        <TableCell align="center"><b>Milestones</b></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.props.grants.map(this.renderRow)}
+                    </TableBody>
+                  </Table>
+              </TableContainer>
+        </React.Fragment>
+    ) ;
+  }
+
+  renderRow = (grant) => {
+    return (
+        <TableRow key={grant.fileName} hover={true}>
+              <TableCell>
+                <a href={'grants/'+grant.fileName}>
+                    {grant.teamName} / {grant.projectName}
+                </a>
+              </TableCell>
+              <TableCell>{this.renderDate(grant.status.acceptDate)}</TableCell>
+              <TableCell align="center">{grant.level}</TableCell>
+              <TableCell align="right">{grant.amount}</TableCell>
+              <TableCell align="center">{grant.paymentCurrency}</TableCell>
+              <TableCell align="center">{grant.numMilestonesDelivered} / {grant.numMilestones}</TableCell>
+        </TableRow>
+    ) ;
+  }
+
+  renderDate = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toISOString().split('T')[0] ;
+  }
 
   render() {
       return (
         <Paper style={{padding:'10px'}}>
-            <Typography component="div" variant="h5">Grants</Typography>
-            <br/>
+            {this.props.grants?this.renderReady():this.renderLoading()}
         </Paper>
       );
   }
